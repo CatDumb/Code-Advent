98e7c41c40a5d83549ab63d71e93c0ea02bad5d6
Nguyễn Minh Khoa <khoa.nguyenbkhcm@hcmut.edu.vn>
2024-12-03 20:01:24 +0700

    day1 part 2 2023 completed

--- 2023/day1_p2.py
+++ 2023/day1_p2.py
@@ -3,6 +3,7 @@ import re
 def main():
     sample_digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     sample_word = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    reverse_sample_word = ["orez", "eno", "owt", "eerht", "ruof", "evif", "xis", "neves", "thgie", "enin"]
     
     with open('day1_input.txt', 'r') as file:
         data = file.read().splitlines()
@@ -10,10 +11,11 @@ def main():
     res = 0
     for line in data:
         first, second = None, None
+        reverse_line = line[::-1]
         
         # Find all matches for digits and words
         matches = re.findall(r'\d|zero|one|two|three|four|five|six|seven|eight|nine', line)
-        
+        reverse_match = re.findall(r'\d|orez|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin', reverse_line)
         if matches:
             # Get the first match
             first_match = matches[0]
@@ -21,13 +23,14 @@ def main():
                 first = int(first_match)
             elif first_match in sample_word:
                 first = sample_word.index(first_match)
-            
+        
+        if reverse_match:
             # Get the last match
-            last_match = matches[-1]
+            last_match = reverse_match[0]
             if last_match in sample_digit:
                 second = int(last_match)
-            elif last_match in sample_word:
-                second = sample_word.index(last_match)
+            elif last_match in reverse_sample_word:
+                second = reverse_sample_word.index(last_match)
         
         if first is not None and second is not None:
             print(line," = ",first * 10 + second)
